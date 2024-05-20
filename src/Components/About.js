597346a220a9fb9278b02ad1943ca163f4f66329
Manishs94 <100223744+Manish-idexcel@users.noreply.github.com>
2024-05-19 23:49:48 -0400

    Update

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,7 +14,7 @@ const About = ({ data }) => {
     var resumeDownload = data.resumedownload;
   }
 
-    const href = () => {
+    const resumeDownload = () => {
       window.open('https://drive.google.com/file/d/1-ZOj34W8dqDzDekdUoR2gOIFKZNjBp2S/view?usp=sharing', '_blank', 'noopener,noreferrer');
     };
 
@@ -51,7 +51,7 @@ const About = ({ data }) => {
             </div>
             <div className="columns download">
               <p>
-                <a onClick={href} className="button">
+                <a onClick={resumeDownload} className="button">
                   <i className="fa fa-download"></i>Download Resume
                 </a>
               </p>
