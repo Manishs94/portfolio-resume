f284c64db7bc64ef960d01d60cb90447de2e109c
Manishs94 <Manishsondhi@live.com>
2025-03-02 19:02:48 -0500

    Update

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,7 +14,7 @@ const About = ({ data }) => {
   }
 
     const handleClick = () => {
-      window.open('');
+      window.open('https://drive.google.com/file/d/1t_JvtBIaTgYOL3RZH_xeN9ojHUXfCUgr/view?usp=sharing');
     };
 
   return (
